777cc9c41b8db7f5ca65ef2be63da5ba1e5d9aea
Tyler Hogsett <tyhogset@microsoft.com>
2021-03-25 12:48:24 -0700

    moved test folder to the root

--- test/world.test.ts
+++ test/world.test.ts
@@ -1,14 +1,14 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { expect } from "chai";
-import { World } from "../src";
+import { expect } from 'chai';
+import { World } from '../lib';
 
-describe("World", () => {
-  const world = new World();
+describe('World', () => {
+    const world = new World();
 
-  it("Hello", () => {
-    expect(world).to.be.not.undefined;
-    world.Hello();
-  });
+    it('Hello', () => {
+        expect(world).to.be.not.undefined;
+        world.Hello();
+    });
 });
