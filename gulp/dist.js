50e733d3df7a76fa361bad5d00824cc6bd5e9946
Tyler Hogsett <tyhogset@microsoft.com>
2021-03-26 17:54:53 -0700

    minor gulp refactors, included serviceConnection

--- gulp/dist.js
+++ gulp/dist.js
@@ -1,53 +1,40 @@
-const { resolve } = require("path");
 const { mkdir, writeJson, pathExists, rm, copy } = require("fs-extra");
 const createTfxRunner = require("./lib/createTfxRunner");
-const binDir = require("./lib/binDir");
 
-const tfxRunner = createTfxRunner();
+const manifest = require("../extension/extension-manifest.json");
+const tasks = require("../extension/task-metadata.json").tasks;
+const serviceConnections = require("../extension/service-connections.json")
+  .contributions;
 
 module.exports = async () => {
-  const rootDir = resolve(__dirname, "..");
-  const distDir = resolve(rootDir, "dist");
-  const extensionSourceFolder = resolve(rootDir, "extension");
-  const manifest = require(resolve(
-    extensionSourceFolder,
-    "extension-manifest.json"
-  ));
-  const tasks = require(resolve(extensionSourceFolder, "task-metadata.json"))
-    .tasks;
+  const tfxRunner = createTfxRunner();
 
-  manifest.contributions = tasks.map((task) => ({
-    id: task.name,
-    type: "ms.vss-distributed-task.task",
-    targets: ["ms.vss-distributed-task.tasks"],
-    properties: {
-      name: "tasks/tool-installer",
-    },
-  }));
+  manifest.contributions = [
+    ...tasks.map((task) => ({
+      id: task.name,
+      type: "ms.vss-distributed-task.task",
+      targets: ["ms.vss-distributed-task.tasks"],
+      properties: {
+        name: "tasks/tool-installer",
+      },
+    })),
+    ...serviceConnections,
+  ];
 
-  const extensionBinFolder = resolve(binDir, "extension");
-  if (await pathExists(extensionBinFolder)) {
-    await new Promise((resolve) =>
-      rm(extensionBinFolder, { recursive: true }, resolve)
-    );
+  const bin = "bin/extension";
+  if (await pathExists(bin)) {
+    await new Promise((resolve) => rm(bin, { recursive: true }, resolve));
   }
-  await mkdir(extensionBinFolder,  {recursive: true});
-  await writeJson(resolve(extensionBinFolder, "vss-extension.json"), manifest, {
+  await mkdir(bin, { recursive: true });
+  await writeJson(`${bin}/vss-extension.json`, manifest, {
     spaces: 2,
   });
-  await copy(
-    resolve(extensionSourceFolder, "assets"),
-    resolve(extensionBinFolder, "assets"),
-    {
-      recursive: true,
-    }
-  );
-  await copy(
-    resolve(rootDir, "README.md"),
-    resolve(extensionBinFolder, "overview.md")
-  );
+  await copy("extension/assets", `${bin}/assets`, {
+    recursive: true,
+  });
+  await copy("README.md", `${bin}/overview.md`);
   await tfxRunner.createExtension({
-    root: extensionBinFolder,
-    outputPath: resolve(distDir),
+    root: bin,
+    outputPath: "dist",
   });
 };
