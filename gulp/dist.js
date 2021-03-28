8725458f0825bb982edd913a3254375ddba7ca24
Tyler Hogsett <tyhogset@microsoft.com>
2021-03-27 21:14:07 -0700

    added ability to override version number

--- gulp/dist.js
+++ gulp/dist.js
@@ -1,13 +1,51 @@
-const { mkdir, writeJson, pathExists, rm, copy } = require("fs-extra");
+const {
+  mkdir,
+  writeJson,
+  pathExists,
+  rm,
+  copy,
+  existsSync,
+} = require("fs-extra");
 const createTfxRunner = require("./lib/createTfxRunner");
 
-const manifest = require("../extension/extension-manifest.json");
-const tasks = require("../extension/task-metadata.json").tasks;
-const serviceConnections = require("../extension/service-connections.json")
-  .contributions;
+const { argv } = require("process");
+const { ArgumentParser } = require("argparse");
 
 module.exports = async () => {
-  const tfxRunner = createTfxRunner();
+  const manifest = require("../extension/extension-manifest.json");
+
+  setVersion(manifest);
+  setContributions(manifest);
+
+  await generateVsix(manifest);
+};
+
+function setVersion(manifest) {
+  const currentVersionParts = manifest.version.split(".");
+  const [currentMajor, currentMinor, currentPatch] = currentVersionParts;
+
+  const parser = new ArgumentParser();
+  parser.add_argument("--major", {
+    type: "int",
+    default: currentMajor,
+  });
+  parser.add_argument("--minor", {
+    type: "int",
+    default: currentMinor,
+  });
+  parser.add_argument("--patch", {
+    type: "int",
+    default: currentPatch,
+  });
+  const { major, minor, patch } = parser.parse_args(argv.slice(3));
+
+  manifest.version = `${major}.${minor}.${patch}`;
+}
+
+function setContributions(manifest) {
+  const tasks = require("../extension/task-metadata.json").tasks;
+  const serviceConnections = require("../extension/service-connections.json")
+    .contributions;
 
   manifest.contributions = [
     ...tasks.map((task) => ({
@@ -20,6 +58,10 @@ module.exports = async () => {
     })),
     ...serviceConnections,
   ];
+}
+
+async function generateVsix(manifest) {
+  const tfxRunner = createTfxRunner();
 
   const bin = "bin/extension";
   if (await pathExists(bin)) {
@@ -33,8 +75,9 @@ module.exports = async () => {
     recursive: true,
   });
   await copy("README.md", `${bin}/overview.md`);
+  if (existsSync("dist")) rm("dist", { recursive: true });
   await tfxRunner.createExtension({
     root: bin,
     outputPath: "dist",
   });
-};
+}
