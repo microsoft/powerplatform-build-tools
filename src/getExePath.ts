4ff7074703fab7508202a939b36dc288637e3bbd
Tyler Hogsett <tyhogset@microsoft.com>
2021-03-29 10:10:30 -0700

    moved tasks to src folder

--- src/getExePath.ts
+++ src/getExePath.ts
@@ -4,17 +4,17 @@ export default function getExePath(...relativePath: string[]): string {
   // in mocha, __dirname resolves to the src folder of the .ts file,
   // but when running the .js file directly, e.g. from the /dist folder, it will be from that folder
   const currentDirectory = resolve(__dirname);
-  let outDirRoot: string;
+  let binDir: string;
   switch (basename(currentDirectory)) {
     case "src":
     case "out":
-      outDirRoot = resolve(currentDirectory, '..', 'out');
+      binDir = resolve(currentDirectory, '..', 'bin');
       break;
     default:
       throw Error(
-        `ExeRunner: cannot resolve outDirRoot running from this location: ${dirname}`
+        `ExeRunner: cannot resolve binDir running from this location: ${dirname}`
       );
   }
 
-  return resolve(outDirRoot, ...relativePath);
+  return resolve(binDir, ...relativePath);
 }
