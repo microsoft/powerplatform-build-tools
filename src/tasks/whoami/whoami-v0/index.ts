9b2a59cf5885160d05fbf21fa39572e332e3a374
Tyler Hogsett <tyhogset@microsoft.com>
2021-03-29 09:59:34 -0700

    fixed refs in whoami index

--- src/tasks/whoami/whoami-v0/index.ts
+++ src/tasks/whoami/whoami-v0/index.ts
@@ -1,13 +1,13 @@
 import tl = require("azure-pipelines-task-lib/task");
 import { PacRunner } from "@microsoft/powerplatform-cli-wrapper";
-import { AuthHandler } from "../../../src/auth/authHandler";
-import createCliWrapperPacAuthenticator from "../../../src/auth/createCliWrapperPacAuthenticator";
-import createAzurePipelinesPacRunner from "../../../src/createAzurePipelinesPacRunner";
+import { AuthHandler } from "../../../auth/authHandler";
+import createCliWrapperPacAuthenticator from "../../../auth/createCliWrapperPacAuthenticator";
+import createAzurePipelinesPacRunner from "../../../createAzurePipelinesPacRunner";
 
 export async function run(pacFactory: () => PacRunner): Promise<void> {
   try {
     // If all tasks allow same auth types this needs to be made generic
-    let authenticationType: string = tl.getInput(
+    const authenticationType: string = tl.getInput(
       "authenticationType",
       true
     ) as string;
