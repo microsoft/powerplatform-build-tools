724443d37e9c56e61fd134cdd3c427e08f54322f
Tyler Hogsett <tyhogset@microsoft.com>
2021-03-29 09:13:30 -0700

    renamed classes to context of Azure DevOps

--- extension/whoami/whoami-v0/index.ts
+++ extension/whoami/whoami-v0/index.ts
@@ -2,21 +2,21 @@ import tl = require("azure-pipelines-task-lib/task");
 import { PacRunner } from "@microsoft/powerplatform-cli-wrapper";
 import { AuthHandler } from "../../../src/auth/authHandler";
 import createCliWrapperPacAuthenticator from "../../../src/auth/createCliWrapperPacAuthenticator";
-import createActionsPacRunner from "../../../src/createActionsPacRunner";
-
+import createAzureDevOpsPacRunner from "../../../src/createAzureDevOpsPacRunner";
 
 export async function run(pacFactory: () => PacRunner): Promise<void> {
   try {
-
     // If all tasks allow same auth types this needs to be made generic
-    let authenticationType: string = tl.getInput('authenticationType', true) as string;
+    let authenticationType: string = tl.getInput(
+      "authenticationType",
+      true
+    ) as string;
 
     const pac = pacFactory();
     const authenticator = createCliWrapperPacAuthenticator(pac);
     await new AuthHandler(authenticator).authenticate(authenticationType);
 
     await pac.whoAmI();
-
   } catch (error) {
     if (error instanceof Error) {
       tl.setResult(tl.TaskResult.Failed, `${error.message}`);
@@ -27,5 +27,4 @@ export async function run(pacFactory: () => PacRunner): Promise<void> {
   }
 }
 
-run(() => createActionsPacRunner());
-
+run(() => createAzureDevOpsPacRunner());
--- src/taskLogger.ts
+++ src/taskLogger.ts
@@ -1,7 +1,7 @@
 import task = require("azure-pipelines-task-lib/task");
 import { Logger } from "@microsoft/powerplatform-cli-wrapper";
 
-export class ActionLogger implements Logger {
+export class TaskLogger implements Logger {
   info(...args: string[]): void {
     task.debug(args.join());
   }
