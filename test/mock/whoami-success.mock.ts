0030329edefe129db0b63c77d397828d188c9582
Tyler Hogsett <tyhogset@microsoft.com>
2021-03-29 10:10:30 -0700

    moved tasks to src folder

--- test/mock/whoami-success.mock.ts
+++ test/mock/whoami-success.mock.ts
@@ -1,8 +1,7 @@
-import ma = require('azure-pipelines-task-lib/mock-answer');
 import tmrm = require('azure-pipelines-task-lib/mock-run');
 import path = require('path');
 
-const taskPath = path.join(__dirname, '..', '..', 'extension', 'whoami', 'whoami-v0', 'index.js');
+const taskPath = path.join(__dirname, '..', '..', 'out', 'tasks', 'whoami', 'whoami-v0', 'index.js');
 const tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
 
 tmr.setInput('authenticationType', 'SPN');
