f9aceb399ddae9af89f7270b64cd2f934e8026af
Tyler Hogsett <tyhogset@microsoft.com>
2021-03-29 13:15:28 -0700

    work on getting the inputs to feed

--- test/mock/whoami-success.mock.ts
+++ test/mock/whoami-success.mock.ts
@@ -1,12 +1,13 @@
 import tmrm = require('azure-pipelines-task-lib/mock-run');
+import { env } from 'process';
 import path = require('path');
 
 const taskPath = path.join(__dirname, '..', '..', 'out', 'tasks', 'whoami', 'whoami-v0', 'index.js');
 const tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
 
 tmr.setInput('authenticationType', 'SPN');
-tmr.setInput('url', '');
-tmr.setInput('username', '');
-tmr.setInput('password', '');
+tmr.setInput('url', env.URL ?? "");
+tmr.setInput('username', env.USERNAME ?? "");
+tmr.setInput('password', env.PASSWORD ?? "");
 
 tmr.run();
