d7b13eb870dbed3f9f1c29c49d6452ea7c139a2b
Tyler Hogsett <tyhogset@microsoft.com>
2021-03-29 09:13:30 -0700

    renamed classes to context of Azure DevOps

---
+++ src/createAzureDevOpsPacRunner.ts
@@ -0,0 +1,18 @@
+import {
+  createPacRunner,
+  PacRunner,
+} from "@microsoft/powerplatform-cli-wrapper";
+import { cwd } from "process";
+import { platform } from "os";
+import { TaskLogger } from "./taskLogger";
+import getExePath from "./getExePath";
+
+export default function createAzureDevOpsPacRunner(): PacRunner {
+  return createPacRunner(
+    cwd(),
+    platform() === "win32"
+      ? getExePath("pac", "tools", "pac.exe")
+      : getExePath("pac_linux", "tools", "pac"),
+    new TaskLogger()
+  );
+}
