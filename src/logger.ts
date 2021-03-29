f51b7d2646dca3e8990e5f24764f43d6f811b915
Tyler Hogsett <tyhogset@microsoft.com>
2021-03-29 09:06:24 -0700

    use Logger from cli-wrapper

--- src/logger.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-export interface Logger {
-    info(...args: string[]): void;
-    warn(...args: string[]): void;
-    error(...args: string[]): void;
-}
