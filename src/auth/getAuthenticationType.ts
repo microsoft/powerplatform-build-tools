7f0d52f2750e99decbd6dff22c98fdecf2f2100b
Tyler Hogsett <tyhogset@microsoft.com>
2021-03-31 00:21:37 -0700

    removed extraneous auth methods

--- src/auth/getAuthenticationType.ts
+++
@@ -1,6 +0,0 @@
-import task = require("azure-pipelines-task-lib/task");
-import AuthenticationType from "./AuthenticationType";
-
-export default function getAuthenticationType(): AuthenticationType {
-  return task.getInput("authenticationType") as AuthenticationType;
-}
