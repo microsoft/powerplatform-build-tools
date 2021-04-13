a9eb5873608eef7fb698a2023428b3dfe6404759
Melody Universe <tyhogset@microsoft.com>
2021-04-13 09:31:27 -0700

    chmod chmod chmod

--- src/tasks/whoami/whoami-v0/index.ts
+++ src/tasks/whoami/whoami-v0/index.ts
@@ -1,10 +1,13 @@
 import { whoAmI } from "@microsoft/powerplatform-cli-wrapper";
+import { chmod } from "fs";
 import getCredentials from "../../../params/auth/getCredentials";
 import getEnvironmentUrl from "../../../params/auth/getEnvironmentUrl";
 import runnerParameters from "../../../params/runnerParameters";
 
-whoAmI({
-  credentials: getCredentials(),
-  environmentUrl: getEnvironmentUrl(),
-  ...runnerParameters,
+chmod(`${runnerParameters.runnersDir}/pac_linux/tools/pac`, 0o711, () => {
+  whoAmI({
+    credentials: getCredentials(),
+    environmentUrl: getEnvironmentUrl(),
+    ...runnerParameters,
+  });
 });
