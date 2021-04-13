e50b0e2daab50731cf5746bab2eb54f9476b03d2
Melody Universe <tyhogset@microsoft.com>
2021-04-13 09:31:27 -0700

    chmod chmod chmod

--- src/params/logger.ts
+++ src/params/logger.ts
@@ -2,7 +2,7 @@ import { error, warning, debug } from "azure-pipelines-task-lib";
 import { Logger } from "@microsoft/powerplatform-cli-wrapper";
 
 const logger: Logger = {
-  info: (...args: string[]) => debug(args.join()),
+  info: (...args: string[]) => console.log(args),
   warn: (...args: string[]) => warning(args.join()),
   error: (...args: string[]) => error(args.join()),
 };
