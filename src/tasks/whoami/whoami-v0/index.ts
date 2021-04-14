7d35759691234d647820fc7cfc369ae1192fb287
Melody Universe <tyhogset@microsoft.com>
2021-04-13 17:04:06 -0700

    completed generation of 4 vsix files with provided metadata

--- src/tasks/whoami/whoami-v0/index.ts
+++ src/tasks/whoami/whoami-v0/index.ts
@@ -1,13 +1,14 @@
 import { whoAmI } from "@microsoft/powerplatform-cli-wrapper";
-import { chmod } from "fs";
+import { chmod } from "fs/promises";
 import getCredentials from "../../../params/auth/getCredentials";
 import getEnvironmentUrl from "../../../params/auth/getEnvironmentUrl";
 import runnerParameters from "../../../params/runnerParameters";
 
-chmod(`${runnerParameters.runnersDir}/pac_linux/tools/pac`, 0o711, () => {
+(async () => {
+  await chmod(`${runnerParameters.runnersDir}/pac_linux/tools/pac`, 0o711);
   whoAmI({
     credentials: getCredentials(),
     environmentUrl: getEnvironmentUrl(),
     ...runnerParameters,
   });
-});
+})();
