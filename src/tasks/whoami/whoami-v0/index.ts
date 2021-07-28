b44687d11752ca788e5a00c8e5eac4645b10de13
Praveen Yelleswarapu <vyelleswarap@microsoft.com>
2021-07-27 23:23:31 -0700

    Updated whoami task and added test infrastructure

--- src/tasks/whoami/whoami-v0/index.ts
+++ src/tasks/whoami/whoami-v0/index.ts
@@ -1,14 +1,16 @@
-import { whoAmI } from "@microsoft/powerplatform-cli-wrapper";
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { whoAmI } from "@microsoft/powerplatform-cli-wrapper/dist/actions";
 import { chmod } from "fs/promises";
-import getCredentials from "../../../params/auth/getCredentials";
-import getEnvironmentUrl from "../../../params/auth/getEnvironmentUrl";
-import runnerParameters from "../../../params/runnerParameters";
+import { getCredentials } from "../../../params/auth/getCredentials";
+import { getEnvironmentUrl } from "../../../params/auth/getEnvironmentUrl";
+import { runnerParameters } from "../../../params/runnerParameters";
 
 (async () => {
   await chmod(`${runnerParameters.runnersDir}/pac_linux/tools/pac`, 0o711);
   whoAmI({
     credentials: getCredentials(),
-    environmentUrl: getEnvironmentUrl(),
-    ...runnerParameters,
-  });
+    environmentUrl: getEnvironmentUrl()
+  }, runnerParameters);
 })();
