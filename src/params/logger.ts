cfb84ce2556908dbe0d216f7133bdc2270937c3f
Praveen Yelleswarapu <71529413+vyelleswarap@users.noreply.github.com>
2021-08-13 16:55:00 -0700

    Updated whoami task and added test infrastructure (#20)

--- src/params/logger.ts
+++ src/params/logger.ts
@@ -1,10 +1,14 @@
-import { error, warning } from "azure-pipelines-task-lib";
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { error, warning, debug } from "azure-pipelines-task-lib";
 import { Logger } from "@microsoft/powerplatform-cli-wrapper";
 
 const logger: Logger = {
-  info: (...args: string[]) => console.log(args),
+  log: (...args: string[]) => console.log(args),
   warn: (...args: string[]) => warning(args.join()),
   error: (...args: string[]) => error(args.join()),
+  debug: (...args: string[]) => debug(args.join())
 };
 
 export default logger;
