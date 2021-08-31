2b964efa85b3b79b0fbbac7de36c0c3b71eb95ae
Praveen Yelleswarapu <vyelleswarap@microsoft.com>
2021-08-31 03:18:53 -0700

    Added export solution

---
+++ src/tasks/export-solution/export-solution-v0/index.ts
@@ -0,0 +1,38 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { exportSolution } from "@microsoft/powerplatform-cli-wrapper/dist/actions";
+import { BuildToolsHost } from "../../../host/BuildToolsHost";
+import { TaskParser } from "../../../parser/TaskParser";
+import { getCredentials } from "../../../params/auth/getCredentials";
+import { getEnvironmentUrl } from "../../../params/auth/getEnvironmentUrl";
+import { runnerParameters } from "../../../params/runnerParameters";
+import { AzurePipelineTaskDefiniton } from "../../../parser/AzurePipelineDefinitions";
+import * as taskDefinitionData from "../../export-solution/export-solution-v0/task.json";
+
+(async () => {
+  const taskParser = new TaskParser();
+  const parameterMap = taskParser.getHostParameterEntries((taskDefinitionData as unknown) as AzurePipelineTaskDefiniton);
+
+  await exportSolution({
+    credentials: getCredentials(),
+    environmentUrl: getEnvironmentUrl(),
+    name: parameterMap['SolutionName'],
+    path: parameterMap['SolutionOutputFile'],
+    managed: parameterMap['Managed'],
+    targetVersion: parameterMap['TargetVersion'],
+    async: parameterMap['AsyncOperation'],
+    maxAsyncWaitTimeInMin: parameterMap['MaxAsyncWaitTime'],
+    autoNumberSettings: parameterMap['ExportAutoNumberingSettings'],
+    calenderSettings: parameterMap['ExportCalendarSettings'],
+    customizationSettings: parameterMap['ExportCustomizationSettings'],
+    emailTrackingSettings: parameterMap['ExportEmailTrackingSettings'],
+    externalApplicationSettings: parameterMap['ExportExternalApplicationSettings'],
+    generalSettings: parameterMap['ExportGeneralSettings'],
+    isvConfig: parameterMap['ExportIsvConfig'],
+    marketingSettings: parameterMap['ExportMarketingSettings'],
+    outlookSynchronizationSettings: parameterMap['ExportOutlookSynchronizationSettings'],
+    relationshipRoles: parameterMap['ExportRelationshipRoles'],
+    sales: parameterMap['ExportSales'],
+  }, runnerParameters, new BuildToolsHost());
+})();
---
+++ test/actions/export-solution.test.ts
@@ -0,0 +1,65 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { should, use } from "chai";
+import { stubInterface } from "ts-sinon";
+import * as sinonChai from "sinon-chai";
+import rewiremock from "../rewiremock";
+import { restore, stub } from "sinon";
+import { mockEnvironmentUrl } from "./mockData";
+import { RunnerParameters, UsernamePassword } from "@microsoft/powerplatform-cli-wrapper";
+import Sinon = require("sinon");
+import { BuildToolsHost } from "../../src/host/BuildToolsHost";
+should();
+use(sinonChai);
+
+describe("export-solution tests", () => {
+  let exportSolutionStub: Sinon.SinonStub<any[], any>;
+  let runnerParameters: RunnerParameters;
+  let credentials: UsernamePassword;
+
+  beforeEach(() => {
+    exportSolutionStub = stub();
+    runnerParameters = stubInterface<RunnerParameters>();
+    credentials = stubInterface<UsernamePassword>();
+  });
+  afterEach(() => restore());
+
+  async function callActionWithMocks(): Promise<void> {
+    await rewiremock.around(() => import("../../src/tasks/export-solution/export-solution-v0/index"),
+      (mock) => {
+        mock(() => import("@microsoft/powerplatform-cli-wrapper/dist/actions")).with({ exportSolution: exportSolutionStub });
+        mock(() => import("../../src/params/auth/getCredentials")).with({ getCredentials: () => credentials });
+        mock(() => import("../../src/params/auth/getEnvironmentUrl")).with({ getEnvironmentUrl: () => mockEnvironmentUrl });
+        mock(() => import("../../src/params/runnerParameters")).with({ runnerParameters: runnerParameters });
+      });
+  }
+
+  it("fetches parameters from index.ts, calls exportSolutionStub properly", async () => {
+
+    await callActionWithMocks();
+
+    exportSolutionStub.should.have.been.calledOnceWithExactly({
+      credentials: credentials,
+      environmentUrl: mockEnvironmentUrl,
+      name: { name: 'SolutionName', required: true, defaultValue: undefined },
+      path: { name: 'SolutionOutputFile', required: true, defaultValue: undefined },
+      managed: { name: 'Managed', required: false, defaultValue: false },
+      targetVersion: { name: 'TargetVersion', required: false, defaultValue: undefined },
+      async: { name: 'AsyncOperation', required: true, defaultValue: true },
+      maxAsyncWaitTimeInMin: { name: 'MaxAsyncWaitTime', required: true, defaultValue: '60' },
+      autoNumberSettings: { name: 'ExportAutoNumberingSettings', required: false, defaultValue: false },
+      calenderSettings: { name: 'ExportCalendarSettings', required: false, defaultValue: false },
+      customizationSettings: { name: 'ExportCustomizationSettings', required: false, defaultValue: false },
+      emailTrackingSettings: { name: 'ExportEmailTrackingSettings', required: false, defaultValue: false },
+      externalApplicationSettings: { name: 'ExportExternalApplicationSettings', required: false, defaultValue: false },
+      generalSettings: { name: 'ExportGeneralSettings', required: false, defaultValue: false },
+      isvConfig: { name: 'ExportIsvConfig', required: false, defaultValue: false },
+      marketingSettings: { name: 'ExportMarketingSettings', required: false, defaultValue: false },
+      outlookSynchronizationSettings: { name: 'ExportOutlookSynchronizationSettings', required: false, defaultValue: false },
+      relationshipRoles: { name: 'ExportRelationshipRoles', required: false, defaultValue: false },
+      sales: { name: 'ExportSales', required: false, defaultValue: false },
+    }, runnerParameters, new BuildToolsHost());
+  });
+});
