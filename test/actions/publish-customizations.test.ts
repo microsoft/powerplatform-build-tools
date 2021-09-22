80424d909134b15b81452c01f0360fe0ac2ec032
Peterson Nguyen <penguy@microsoft.com>
2021-09-22 11:29:40 -0700

    Adding publish solution and deploy package

---
+++ test/actions/publish-customizations.test.ts
@@ -0,0 +1,49 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { should, use } from "chai";
+import { stubInterface } from "ts-sinon";
+import * as sinonChai from "sinon-chai";
+import rewiremock from "../rewiremock";
+import { fake, restore, stub } from "sinon";
+import { mockEnvironmentUrl } from "./mockData";
+import { RunnerParameters, UsernamePassword } from "@microsoft/powerplatform-cli-wrapper";
+import Sinon = require("sinon");
+should();
+use(sinonChai);
+
+describe("publish customizations tests", () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let publishSolutionStub: Sinon.SinonStub<any[], any>;
+  let runnerParameters: RunnerParameters;
+  let credentials: UsernamePassword;
+
+  beforeEach(() => {
+    publishSolutionStub = stub();
+    runnerParameters = stubInterface<RunnerParameters>();
+    credentials = stubInterface<UsernamePassword>();
+  });
+  afterEach(() => restore());
+
+  async function callActionWithMocks(): Promise<void> {
+    await rewiremock.around(
+      () => import("../../src/tasks/publish-customizations/publish-customizations-v0/index"),
+      (mock) => {
+        mock(() => import("@microsoft/powerplatform-cli-wrapper/dist/actions")).with({ publishSolution: publishSolutionStub });
+        mock(() => import("../../src/params/auth/getCredentials")).with({ getCredentials: () => credentials });
+        mock(() => import("../../src/params/auth/getEnvironmentUrl")).with({ getEnvironmentUrl: () => mockEnvironmentUrl });
+        mock(() => import("fs/promises")).with({ chmod: fake() });
+        mock(() => import("../../src/params/runnerParameters")).with({ runnerParameters: runnerParameters });
+      });
+  }
+
+  it("calls publish solution", async () => {
+
+    await callActionWithMocks();
+
+    publishSolutionStub.should.have.been.calledOnceWithExactly({
+      credentials: credentials,
+      environmentUrl: mockEnvironmentUrl
+    }, runnerParameters);
+  });
+});
