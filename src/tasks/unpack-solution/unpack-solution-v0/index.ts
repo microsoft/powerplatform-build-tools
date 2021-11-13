4f496876f68264473d1b2d1fb26a256dc810055c
david@DAVID-PC <davidjen@microsoft.com>
2021-11-12 16:31:15 -0800

    remove authN/envUrl from pack/unpack solution

--- src/tasks/unpack-solution/unpack-solution-v0/index.ts
+++ src/tasks/unpack-solution/unpack-solution-v0/index.ts
@@ -6,8 +6,6 @@ import { unpackSolution } from "@microsoft/powerplatform-cli-wrapper/dist/action
 import { isRunningOnAgent } from "../../../params/auth/isRunningOnAgent";
 import { BuildToolsHost } from "../../../host/BuildToolsHost";
 import { TaskParser } from "../../../parser/TaskParser";
-import { getCredentials } from "../../../params/auth/getCredentials";
-import { getEnvironmentUrl } from "../../../params/auth/getEnvironmentUrl";
 import { AzurePipelineTaskDefiniton } from "../../../parser/AzurePipelineDefinitions";
 import * as taskDefinitionData from "./task.json";
 import { BuildToolsRunnerParams } from "../../../host/BuildToolsRunnerParams";
@@ -16,7 +14,7 @@ import { BuildToolsRunnerParams } from "../../../host/BuildToolsRunnerParams";
   if (isRunningOnAgent()) {
       await main();
   }
-  })().catch(error => {
+})().catch(error => {
   tl.setResult(tl.TaskResult.Failed, error);
 });
 
@@ -25,8 +23,6 @@ export async function main(): Promise<void> {
   const parameterMap = taskParser.getHostParameterEntries((taskDefinitionData as unknown) as AzurePipelineTaskDefiniton);
 
   await unpackSolution({
-    credentials: getCredentials(),
-    environmentUrl: getEnvironmentUrl(),
     solutionZipFile: parameterMap['SolutionInputFile'],
     sourceFolder: parameterMap['SolutionTargetFolder'],
     solutionType: parameterMap['SolutionType'],
