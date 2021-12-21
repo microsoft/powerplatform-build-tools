bdcd8e26871ab49ae2c0e20e9ef2b816c48bfcc9
Peterson Nguyen <penguy@microsoft.com>
2021-12-21 12:00:17 -0800

    Add set online version solution task

---
+++ test/actions/online-version-solution.test.ts
@@ -0,0 +1,51 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { should, use } from "chai";
+import { stubInterface } from "ts-sinon";
+import * as sinonChai from "sinon-chai";
+import rewiremock from "../rewiremock";
+import { restore, stub } from "sinon";
+import { mockEnvironmentUrl } from "./mockData";
+import { UsernamePassword } from "@microsoft/powerplatform-cli-wrapper";
+import Sinon = require("sinon");
+import { BuildToolsHost } from "../../src/host/BuildToolsHost";
+import { BuildToolsRunnerParams } from "../../src/host/BuildToolsRunnerParams";
+
+should();
+use(sinonChai);
+
+describe("set online version solution test", () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let onlineVersionSolutionStub: Sinon.SinonStub<any[], any>;
+  let credentials: UsernamePassword;
+
+  beforeEach(() => {
+    onlineVersionSolutionStub = stub();
+    credentials = stubInterface<UsernamePassword>();
+  });
+  afterEach(() => restore());
+
+  async function callActionWithMocks(): Promise<void> {
+    const apply = await rewiremock.around(
+      () => import("../../src/tasks/online-version-solution/online-version-solution-v0/index"),
+      (mock) => {
+        mock(() => import("@microsoft/powerplatform-cli-wrapper/dist/actions")).with({ onlineVersionSolution: onlineVersionSolutionStub });
+        mock(() => import("../../src/params/auth/getCredentials")).with({ getCredentials: () => credentials });
+        mock(() => import("../../src/params/auth/getEnvironmentUrl")).with({ getEnvironmentUrl: () => mockEnvironmentUrl });
+      });
+      apply.main();
+  }
+
+  it("calls set online version solution", async () => {
+
+    await callActionWithMocks();
+
+    onlineVersionSolutionStub.should.have.been.calledOnceWithExactly({
+      credentials: credentials,
+      environmentUrl: mockEnvironmentUrl,
+      name: { name: 'SolutionName', required: true, defaultValue: undefined },
+      version: { name: 'SolutionVersionNumber', required: true, defaultValue: undefined },
+    }, new BuildToolsRunnerParams(), new BuildToolsHost());
+  });
+});
