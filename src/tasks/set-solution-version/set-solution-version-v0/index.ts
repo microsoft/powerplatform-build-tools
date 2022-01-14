886e8038ed85506ca78de3fbf6f7242061911283
penguy-ms <71346301+penguy-ms@users.noreply.github.com>
2022-01-14 10:39:15 -0800

    Add set online version solution task (#58)
    * Add set online version solution task
    
    * Fixing naming and added set soln to comp test

---
+++ src/tasks/set-solution-version/set-solution-version-v0/index.ts
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import * as tl from 'azure-pipelines-task-lib/task';
+import { onlineVersionSolution } from "@microsoft/powerplatform-cli-wrapper/dist/actions";
+import { BuildToolsHost } from "../../../host/BuildToolsHost";
+import { TaskParser } from "../../../parser/TaskParser";
+import { getCredentials } from "../../../params/auth/getCredentials";
+import { getEnvironmentUrl } from "../../../params/auth/getEnvironmentUrl";
+import { AzurePipelineTaskDefiniton } from "../../../parser/AzurePipelineDefinitions";
+import * as taskDefinitionData from "./task.json";
+import { isRunningOnAgent } from "../../../params/auth/isRunningOnAgent";
+import { BuildToolsRunnerParams } from "../../../host/BuildToolsRunnerParams";
+
+(async () => {
+  if (isRunningOnAgent()) {
+    await main();
+  }
+})().catch(error => {
+  tl.setResult(tl.TaskResult.Failed, error);
+});
+
+export async function main(): Promise<void> {
+  const taskParser = new TaskParser();
+  const parameterMap = taskParser.getHostParameterEntries((taskDefinitionData as unknown) as AzurePipelineTaskDefiniton);
+
+  await onlineVersionSolution({
+    credentials: getCredentials(),
+    environmentUrl: getEnvironmentUrl(),
+    name: parameterMap['SolutionName'],
+    version: parameterMap['SolutionVersionNumber'],
+  }, new BuildToolsRunnerParams(), new BuildToolsHost());
+}
