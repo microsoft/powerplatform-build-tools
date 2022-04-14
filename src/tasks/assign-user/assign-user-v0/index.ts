e71985f8918bfa88d40a47dd584ecd7859fa1918
Paul Breuler <4356863+paulbreuler@users.noreply.github.com>
2022-04-14 16:38:49 -0400

    Add assign-user implementation and test code

---
+++ src/tasks/assign-user/assign-user-v0/index.ts
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import * as tl from 'azure-pipelines-task-lib/task';
+import { assignUser } from "@microsoft/powerplatform-cli-wrapper/dist/actions";
+import { BuildToolsRunnerParams } from "../../../host/BuildToolsRunnerParams";
+import { getCredentials } from "../../../params/auth/getCredentials";
+import { getEnvironmentUrl } from "../../../params/auth/getEnvironmentUrl";
+import { isRunningOnAgent } from '../../../params/auth/isRunningOnAgent';
+import * as taskDefinitionData from "./task.json";
+import { TaskParser } from "../../../parser/TaskParser";
+import { AzurePipelineTaskDefiniton } from 'src/parser/AzurePipelineDefinitions';
+import { BuildToolsHost } from "../../../host/BuildToolsHost";
+
+(async () => {
+  if (isRunningOnAgent()) {
+      await main();
+  }
+})().catch(error => {
+  tl.setResult(tl.TaskResult.Failed, error);
+});
+
+
+export async function main(): Promise<void> {
+  const taskParser = new TaskParser();
+  const parameterMap = taskParser.getHostParameterEntries((taskDefinitionData as unknown) as AzurePipelineTaskDefiniton);
+
+  await assignUser({
+    credentials: getCredentials(),
+    environment: parameterMap['Environment'],
+    objectId: parameterMap['ObjectId'],
+    role: parameterMap['Role']
+  }, new BuildToolsRunnerParams(), new BuildToolsHost());
+}
---
+++ test/actions/assign-user.test.ts
@@ -0,0 +1,48 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { should, use } from "chai";
+import { stubInterface } from "ts-sinon";
+import * as sinonChai from "sinon-chai";
+import rewiremock from "../rewiremock";
+import { restore, stub } from "sinon";
+import { UsernamePassword } from "@microsoft/powerplatform-cli-wrapper";
+import Sinon = require("sinon");
+import { BuildToolsHost } from "../../src/host/BuildToolsHost";
+import { BuildToolsRunnerParams } from "../../src/host/BuildToolsRunnerParams";
+
+should();
+use(sinonChai);
+
+describe("set assign user to target environment", () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let assignUserStub: Sinon.SinonStub<any[], any>;
+  let credentials: UsernamePassword;
+
+  beforeEach(() => {
+    assignUserStub = stub();
+    credentials = stubInterface<UsernamePassword>();
+  });
+  afterEach(() => restore());
+
+  async function callActionWithMocks(): Promise<void> {
+    const mockedModule = await rewiremock.around(() => import("../../src/tasks/assign-user/assign-user-v0/index"),
+      (mock) => {
+        mock(() => import("@microsoft/powerplatform-cli-wrapper/dist/actions")).with({ assignUser: assignUserStub });
+        mock(() => import("../../src/params/auth/getCredentials")).with({ getCredentials: () => credentials });
+      });
+    await mockedModule.main();
+  }
+
+  it("calls assign user action", async () => {
+
+    await callActionWithMocks();
+
+    assignUserStub.should.have.been.calledOnceWithExactly({
+      credentials: credentials,
+      environment: { name: 'Environment', required: true, defaultValue: '$(BuildTools.EnvironmentUrl)' },
+      objectId: { name: 'ObjectId', required: true, defaultValue: undefined },
+      role: { name: 'Role', required: true, defaultValue: undefined },
+    }, new BuildToolsRunnerParams(), new BuildToolsHost());
+  });
+});
