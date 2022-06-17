981441209e988eff61f0a9f6806740c46c26b987
Paul Breuler <4356863+paulbreuler@users.noreply.github.com>
2022-06-17 16:58:53 -0400

    first pass convert to esbuild

--- gulp/compile.js
+++ gulp/compile.js
@@ -1,6 +1,8 @@
-const { statSync } = require("fs-extra");
 const path = require("path");
-const webpack = require("webpack");
+const fs = require('fs-extra');
+const esbuild = require('esbuild');
+const find = require("find");
+const distdir = path.resolve('./dist');
 
 module.exports = function compile() {
   // detect if running on an AzDevOps build agent
@@ -12,29 +14,41 @@ module.exports = function compile() {
     console.log(`local build: ${packageJson.version}`);
   }
   return new Promise((resolve) => {
-    const config = require("../webpack.config");
-    webpack(config).run(onBuild(resolve));
+    fs.emptyDirSync(distdir);
+
+    const tasks = find.fileSync(/tasks[/\\].*[/\\]index.ts$/, "src");
+    tasks.map((task, idx) =>{
+      var baseName = path.basename(path.dirname(task));
+      var taskName = path.basename(path.join(task, "../.."))
+      const taskDistDir = path.resolve(distdir, taskName, baseName);
+      console.info(`packaging ${idx} "${task}" into ./dist folder ...`);
+      esbuild.buildSync({
+        entryPoints: [`./${task}`],
+        outfile: path.join(taskDistDir, "index.js"),
+        platform: "node",
+    })
+    })
   });
 };
 
-function onBuild(done) {
-  return function (err, result) {
-    if (err) {
-      console.error(`Webpack error:\n${err}`);
-      if (done) {
-        done();
-      }
-    } else {
-      result.stats.forEach((stats) => {
-        Object.keys(stats.compilation.assets).forEach(function (key) {
-          console.log(`Webpack: output ${key}`);
-        });
-        const size = statSync(path.join(stats.compilation.outputOptions.path, stats.compilation.outputOptions.filename)).size;
-        console.log(`Webpack: finished, size = ${size}`);
-      });
-      if (done) {
-        done();
-      }
-    }
-  };
-}
+// function onBuild(done) {
+//   return function (err, result) {
+//     if (err) {
+//       console.error(`Webpack error:\n${err}`);
+//       if (done) {
+//         done();
+//       }
+//     } else {
+//       result.stats.forEach((stats) => {
+//         Object.keys(stats.compilation.assets).forEach(function (key) {
+//           console.log(`Webpack: output ${key}`);
+//         });
+//         const size = statSync(path.join(stats.compilation.outputOptions.path, stats.compilation.outputOptions.filename)).size;
+//         console.log(`Webpack: finished, size = ${size}`);
+//       });
+//       if (done) {
+//         done();
+//       }
+//     }
+//   };
+// }
