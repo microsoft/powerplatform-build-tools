85c292b74c7daee0f5543dbb15f62f19854545f2
Andrew Petrochuk <anpetroc@microsoft.com>
2022-09-01 09:56:05 -0700

    Added data import/export

---
+++ src/tasks/export-data/export-data-v2/index.ts
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import * as tl from 'azure-pipelines-task-lib/task';
+import { dataExport } from "@microsoft/powerplatform-cli-wrapper/dist/actions";
+import { BuildToolsRunnerParams } from "../../../host/BuildToolsRunnerParams";
+import { getCredentials } from "../../../params/auth/getCredentials";
+import { isRunningOnAgent } from '../../../params/auth/isRunningOnAgent';
+import * as taskDefinitionData from "./task.json";
+import { TaskParser } from "../../../parser/TaskParser";
+import { AzurePipelineTaskDefiniton } from 'src/parser/AzurePipelineDefinitions';
+import { BuildToolsHost } from "../../../host/BuildToolsHost";
+
+(async () => {
+  if (isRunningOnAgent()) {
+      await main();
+  }
+})().catch(error => {
+  tl.setResult(tl.TaskResult.Failed, error);
+});
+
+
+export async function main(): Promise<void> {
+  const taskParser = new TaskParser();
+  const parameterMap = taskParser.getHostParameterEntries((taskDefinitionData as unknown) as AzurePipelineTaskDefiniton);
+
+  await dataExport({
+    credentials: getCredentials(),
+    schemaFile: parameterMap['SchemaFile'],
+    dataFile: parameterMap['DataFile'],
+    overwrite: parameterMap['Overwrite'],
+    verbose: parameterMap['Verbose'],
+    environment: parameterMap['Environment'],
+  }, new BuildToolsRunnerParams(), new BuildToolsHost());
+}
