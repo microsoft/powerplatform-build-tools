23e49806e3b6956b98b98123f6f875824dae2892
Swapnil Bhalgat <sbhalgat@microsoft.com>
2023-05-10 16:01:44 -0700

    add catalog install, submit and status actions, also add saveResults parameter to solution check task

--- src/tasks/checker/checker-v2/index.ts
+++ src/tasks/checker/checker-v2/index.ts
@@ -44,6 +44,7 @@ export async function main(): Promise<void> {
     artifactStoreName: parameterMap['ArtifactDestinationName'],
     useDefaultPAEndpoint: parameterMap['UseDefaultPACheckerEndpoint'],
     customPAEndpoint: parameterMap['CustomPACheckerEndpoint'],
-    geoInstance: { name: "GeoInstance", required: false, defaultValue: undefined }
+    geoInstance: { name: "GeoInstance", required: false, defaultValue: undefined },
+    saveResults: { name: "SaveResults", required: false, defaultValue: false }
   }, new BuildToolsRunnerParams(), new BuildToolsHost('PowerAppsChecker'));
 }
