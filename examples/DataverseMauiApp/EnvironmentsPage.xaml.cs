0156b9f4a76429e96a8def2505d5202de9ba4a9b
Andrew Petrochuk <anpetroc@microsoft.com>
2023-06-24 19:38:01 -0700

    Improved binding performance

--- examples/DataverseMauiApp/EnvironmentsPage.xaml.cs
+++ examples/DataverseMauiApp/EnvironmentsPage.xaml.cs
@@ -1,20 +1,27 @@
+using System.Diagnostics;
 using System.Windows.Input;
 
 namespace DataverseMauiApp;
 
 public partial class EnvironmentsPage : ContentPage
 {
-	public EnvironmentsPage(EnvironmentsViewModel viewModel)
+  EnvironmentsViewModel _viewModel;
+
+  public EnvironmentsPage(EnvironmentsViewModel viewModel)
 	{
-    BindingContext = viewModel;
 		InitializeComponent();
+    _viewModel = viewModel;
 	}
 
   protected override async void OnNavigatedTo(NavigatedToEventArgs args)
   {
     base.OnNavigatedTo(args);
 
-    await ViewModel.RefreshAsync().ConfigureAwait(false);
+    await _viewModel.RefreshAsync().ConfigureAwait(false);
+
+    await Application.Current.Dispatcher.DispatchAsync(() => { _environments.ItemsSource = _viewModel; }).ConfigureAwait(false);
+
+    Debug.WriteLine("Binding complete");
   }
 
   protected EnvironmentsViewModel ViewModel => BindingContext as EnvironmentsViewModel;
