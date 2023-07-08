a71be8085051cf70a82c6d64f909f590183bb1cf
Andrew Petrochuk <anpetroc@microsoft.com>
2023-07-07 18:41:32 -0700

    Move files

--- examples/VotingApp/VotePcf/vite.config.ts
+++ examples/VotingApp/VotePcf/vite.config.ts
@@ -1,17 +1,50 @@
 // vite.config.js
 import { resolve } from 'path'
+import * as fs from 'node:fs'
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const fileRegex = /\.(xml)$/
+
+function myPlugin() {
+  return {
+    name: 'transform-file',
+
+    closeBundle() {
+      console.warn(`closeBundle copy ${resolve(__dirname, 'src/ControlManifest.Input.xml')}`)
+      fs.copyFileSync(
+        resolve(__dirname, 'src/ControlManifest.Input.xml'),
+        resolve(__dirname, 'dist/ControlManifest.xml'))
+    },
+    transform(src, id) {
+      if (fileRegex.test(id)) {
+        console.log(`Transforming id: ${id}`)
+        return {
+          code: src,
+          map: null, // provide source map if available
+        }
+      }
+    },
+  }
+}
+
 export default defineConfig({
-  root: 'src',
+  plugins: [ myPlugin() ],
+  publicDir: "node_modules/pcf-start",
+  assetsInclude: ['**/*.xml'],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
   build: {
     // Relative to the root
-    outDir: '../dist',
+    outDir: './dist',
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'PCF_bundle',
+      name: 'DPX',
       // the proper extensions will be added
       fileName: 'bundle',
       formats: ['es', 'umd', 'cjs', 'iife']
@@ -28,8 +61,10 @@ export default defineConfig({
           symbols: false
         },
         globals: {
+          'react': 'React',
           '@fluentui/react': 'FluentUIReactv8290',
-        }
+        },
+        assetFileNames: resolve(__dirname, 'src/ControlManifest.Input.xml'),
       }
     }
   },
