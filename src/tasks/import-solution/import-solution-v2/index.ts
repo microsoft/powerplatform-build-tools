ac6da93e2403bc2375663d099466d34f1660f97c
Crash Collison <3751389+tehcrashxor@users.noreply.github.com>
2023-10-20 15:28:48 -0700

    Revert "October Release (#590)" to test release failures (#593)
    This reverts commit 00e7cdee885e5bb85e3c9c5b9dba3d2bcc552fc4.

--- src/tasks/import-solution/import-solution-v2/index.ts
+++ src/tasks/import-solution/import-solution-v2/index.ts
@@ -41,7 +41,6 @@ export async function main(): Promise<void> {
     async: parameterMap['AsyncOperation'],
     maxAsyncWaitTimeInMin: parameterMap['MaxAsyncWaitTime'],
     importAsHolding: parameterMap['HoldingSolution'],
-    stageAndUpgrade: parameterMap['StageAndUpgrade'],
     forceOverwrite: parameterMap['OverwriteUnmanagedCustomizations'],
     publishChanges: parameterMap['PublishCustomizationChanges'],
     skipDependencyCheck: parameterMap['SkipProductUpdateDependencies'],
