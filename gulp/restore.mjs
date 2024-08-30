92e253ecb09a29d734d48d8b2c915abb7efe91e7
Crash Collison <3751389+tehcrashxor@users.noreply.github.com>
2024-08-30 11:09:41 -0700

    Add option to override the nuget feed used by command line argument

--- gulp/restore.mjs
+++ gulp/restore.mjs
@@ -3,15 +3,13 @@ import nugetInstall from "./lib/nugetInstall.mjs";
 
 import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
-const nugetFeeds = require("../nuget.json").feeds;
+const nugetConfig = require("../nuget.json");
 
 // https://docs.microsoft.com/en-us/nuget/api/package-base-address-resource
 // https://dev.azure.com/msazure/One/_packaging?_a=feed&feed=CAP_ISVExp_Tools_Daily
 
 export default gulp.series(
-  ...nugetFeeds
-    .map((feed) =>
-      feed.packages.map((pkg) => async () => nugetInstall(feed, pkg))
-    )
+  ...nugetConfig.packages
+    .map((pkg) => async () => nugetInstall(pkg, nugetConfig.feeds))
     .flat()
 );
